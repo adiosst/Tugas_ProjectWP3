c70a3a4d8ff7c2b8bc9b64c1aee5587da8ddfb02
adiosst <149923915+adiosst@users.noreply.github.com>
2023-11-06 10:25:05 +0700

    Add files via upload

--- application/controllers/admin/Agama.php
+++ application/controllers/admin/Agama.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Nim extends CI_Controller {
+class Agama extends CI_Controller {
 
     public function __construct()
         {
@@ -16,20 +16,20 @@ class Nim extends CI_Controller {
         if(!empty($cek) && $stts=='admin')
         {
             /*layout*/  
-            $data['title']='Daftar Profesi';
-            $data['pointer']="Profesi";
+            $data['title']='Daftar Agama';
+            $data['pointer']="Agama";
             $data['classicon']="fa fa-book";
-            $data['main_bread']="Data Masyarakat";
-            $data['sub_bread']="Daftar Profesi";
-            $data['desc']="Data Master Profesi, Menampilkan data Profesi";
+            $data['main_bread']="Data Agama";
+            $data['sub_bread']="Daftar Agama";
+            $data['desc']="Data Master Agama, Menampilkan data Agama";
 
             /*data yang ditampilkan*/
-            //$data['data_agama'] = $this->Buku_model->getAllData("tb_agama");
-            $data['data_kelas'] = $this->Buku_model->getAllData("tb_kelas");
+            $data['data_agama'] = $this->Buku_model->getAllData("tb_agama");
+            //$data['data_kelas'] = $this->Buku_model->getAllData("tb_kelas");
             //$data['data_agama'] = $this->Buku_model->getAllData("tb_agama");
             //$data['isi']=$this->Anggota_model->get_all();
             //$data['js']=$this->load->view('admin/anggota/js');
-            $tmp['content']=$this->load->view('admin/kelas/View_kelas',$data, TRUE);
+            $tmp['content']=$this->load->view('admin/agama/View_agama',$data, TRUE);
             $this->load->view('admin/layout',$tmp);
         }
         else
@@ -45,33 +45,33 @@ class Nim extends CI_Controller {
         $stts = $this->session->userdata('stts');
         if(!empty($cek) && $stts=='admin')
         {   
-                $this->form_validation->set_rules('kelas', 'kelas', 'trim|required');
+                $this->form_validation->set_rules('agama', 'agama', 'trim|required');
 
                 if($this->form_validation->run()==FALSE)
                 {
-                    $data['title']='Tambah Profesi';
-                    $data['pointer']="Profesi";
+                    $data['title']='Tambah Agama';
+                    $data['pointer']="Agama";
                     $data['classicon']="fa fa-book";
-                    $data['main_bread']="Data Profesi";
-                    $data['sub_bread']="Tambah Profesi";
-                    $data['desc']="form untuk Input data Profesi";
+                    $data['main_bread']="Data Agama";
+                    $data['sub_bread']="Tambah Agama";
+                    $data['desc']="form untuk Input data Agama";
 
                     /*data yang ditampilkan*/
                     //$data['data_provinsi'] = $this->Buku_model->getAllData("tb_provinsi");
-                    $data['data_kelas'] = $this->Buku_model->getAllData("tb_kelas");
-                    //$data['data_agama'] = $this->Buku_model->getAllData("tb_agama");
+                    //$data['data_kelas'] = $this->Buku_model->getAllData("tb_kelas");
+                    $data['data_agama'] = $this->Buku_model->getAllData("tb_agama");
                     //$data['isi']=$this->Anggota_model->get_all();
                     //$data['js']=$this->load->view('admin/anggota/js');
-                    $tmp['content']=$this->load->view('admin/kelas/Create_kelas',$data, TRUE);
+                    $tmp['content']=$this->load->view('admin/agama/Create_agama',$data, TRUE);
                     $this->load->view('admin/layout',$tmp);
                 }
                 else
                 {
-                    $data = array('id_kelas' => '',
-                                  'kelas' => $this->input->post('kelas')
+                    $data = array('id_agama' => '',
+                                  'agama' => $this->input->post('agama')
                                 );
-                    $quer=$this->Buku_model->insertData('tb_kelas',$data);
-                    redirect("admin/Nim","refresh");   
+                    $quer=$this->Buku_model->insertData('tb_agama',$data);
+                    redirect("admin/Agama","refresh");   
                 }
         }
         else
@@ -86,24 +86,24 @@ class Nim extends CI_Controller {
         $stts = $this->session->userdata('stts');
         if(!empty($cek) && $stts=='admin')
         {       
-                $id = $this->input->get('id_kelas',true);    
-                $this->form_validation->set_rules('kelas', 'kelas', 'trim|required');
+                $id = $this->input->get('id_agama',true);    
+                $this->form_validation->set_rules('agama', 'agama', 'trim|required');
 
                 if($this->form_validation->run()==FALSE)
                 {
                     //$data ['err'] = validation_errors();
-                    $data['title']='Edit Kelas';
-                    $data['pointer']="Kelas";
+                    $data['title']='Edit Agama';
+                    $data['pointer']="Agama";
                     $data['classicon']="fa fa-book";
-                    $data['main_bread']="Data Kelas";
-                    $data['sub_bread']="Edit Kelas";
-                    $data['desc']="Form untuk melakukan edit data Kelas";
+                    $data['main_bread']="Data Agama";
+                    $data['sub_bread']="Edit Agama";
+                    $data['desc']="Form untuk melakukan edit data Agama";
 
                     /*data yang ditampilkan*/
                     //$data['provinsi'] = $this->Buku_model->get_detail1('tb_provinsi','id_provinsi',$id);
-                    $data['kelas'] = $this->Buku_model->get_detail1('tb_kelas','id_kelas',$id);
-                    //$data['agama'] = $this->Buku_model->get_detail1('tb_agama','id_agama',$id);
-                    $tmp['content']=$this->load->view('admin/kelas/Edit_kelas',$data,true);
+                    //$data['data_kelas'] = $this->Buku_model->getAllData("tb_kelas");
+                    $data['agama'] = $this->Buku_model->get_detail1('tb_agama','id_agama',$id);
+                    $tmp['content']=$this->load->view('admin/agama/Edit_agama',$data,true);
                     $this->load->view('admin/layout',$tmp);
                 }
         }
@@ -119,34 +119,34 @@ class Nim extends CI_Controller {
         $stts = $this->session->userdata('stts');
         if(!empty($cek) && $stts=='admin')
         {       
-                $id = $this->input->post('id_kelas');    
-                $this->form_validation->set_rules('kelas', 'kelas', 'trim|required');
+                $id = $this->input->post('id_agama');    
+                $this->form_validation->set_rules('agama', 'agama', 'trim|required');
                 if($this->form_validation->run()==FALSE)
                 {
                     //$data ['err'] = validation_errors();
-                    $data['title']='Edit Profesi';
-                    $data['pointer']="Kelas";
+                    $data['title']='Edit Agama';
+                    $data['pointer']="Agama";
                     $data['classicon']="fa fa-book";
-                    $data['main_bread']="Data Kelas";
-                    $data['sub_bread']="Edit Kelas";
-                    $data['desc']="Form untuk melakukan edit Profesi";
+                    $data['main_bread']="Data Agama";
+                    $data['sub_bread']="Edit Agama";
+                    $data['desc']="Form untuk melakukan edit Agama";
 
                     /*data yang ditampilkan*/
-                    //$data['agama'] = $this->Buku_model->get_detail1('tb_agama','id_agama',$id);
-                    $data['kelas'] = $this->Buku_model->get_detail1('tb_kelas','id_kelas',$id);
+                    $data['agama'] = $this->Buku_model->get_detail1('tb_agama','id_agama',$id);
+                    //$data['data_kelas'] = $this->Buku_model->getAllData("tb_kelas");
                     //$data['data_agama'] = $this->Buku_model->getAllData("tb_agama");
                     $tmp['content']=$this->load->view('admin/provinsi/Edit_anggota',$data,true);
                     $this->load->view('admin/layout',$tmp);
                 }
                 else
                 {
-                    $id = $this->input->post('id_kelas');    
-                    $field='id_kelas';
+                    $id = $this->input->post('id_agama');    
+                    $field='id_agama';
                     $data = array(
-                                  'kelas' => $this->input->post('kelas')
+                                  'agama' => $this->input->post('agama')
                                 );
-                    $quer=$this->Buku_model->updateData1('tb_kelas',$data,$field,$id);
-                    redirect("admin/Nim","refresh");   
+                    $quer=$this->Buku_model->updateData1('tb_agama',$data,$field,$id);
+                    redirect("admin/Agama","refresh");   
                 }
         }
         else
@@ -157,18 +157,18 @@ class Nim extends CI_Controller {
         public function delete()
         {
             $data['log']=$this->db->get_where('tb_petugas',array('id_petugas' => $this->session->userdata('username')))->result();
-            $field='id_kelas';
-            $id = $this->input->get('id_kelas',true);    
-            $query = $this->Buku_model->delete('tb_kelas',$field,$id);                   
+            $field='id_agama';
+            $id = $this->input->get('id_agama',true);    
+            $query = $this->Buku_model->delete('tb_agama',$field,$id);                   
             if ($query)
                 {
                     $this->session->set_flashdata("message","berhasil");
-                    redirect("admin/Nim","refresh");
+                    redirect("admin/Agama","refresh");
                 }
             else
                 {
                     $this->session->set_flashdata("message","gagal");
-                    redirect("admin/Nim","refresh");
+                    redirect("admin/Agama","refresh");
                 }
         }
 }
